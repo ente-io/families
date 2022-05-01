2c296c08487b517020c1c814d9d26f2d6f1603ba
Rushikesh Tote <rushi.tote@gmail.com>
2022-05-01 11:19:53 +0530

    added top margin for mobile devices

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -67,7 +67,7 @@ function Landing({ setPage }: { setPage: (page: number) => void }) {
                 container
                 spacing={0}
                 style={{
-                    marginTop: mediaQuery ? '72px' : '0px',
+                    marginTop: mediaQuery ? '72px' : '40px',
                 }}>
                 <Grid item xs={12} md={6}>
                     <ContentContainer mq={mediaQuery ? true : undefined}>
