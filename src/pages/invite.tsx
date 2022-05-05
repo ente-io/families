6794c6290f54254d56ee193364aeb73c5a364f4e
Rushikesh Tote <rushi.tote@gmail.com>
2022-05-05 12:48:16 +0530

    move components to pages

--- src/pages/invite.tsx
+++ src/pages/invite.tsx
@@ -1,11 +1,109 @@
-import React from 'react';
-import InviteAccepted from '../components/InviteAccepted';
+import { Container, Button } from '@mui/material';
+import React, { useContext } from 'react';
+import { ImageContainer } from '../styles/InviteAccepted';
+import theme from '../theme';
+import { convertBytesToHumanReadable } from '../util/common';
+import constants from '../util/strings/constants';
+import { AppContext } from './_app';
 
 function Invite() {
+    const { isSmallerDisplay, familyManagerEmail, totalStorage } =
+        useContext(AppContext);
+
+    const handleClick = () => {
+        window.open('https://web.ente.io', '_self');
+    };
     return (
-        <>
-            <InviteAccepted />
-        </>
+        <Container
+            maxWidth="md"
+            style={{
+                marginTop: '50px',
+            }}>
+            <div
+                style={{
+                    fontSize: '40px',
+                    fontWeight: 700,
+                }}>
+                {constants.CONGRATULATIONS}
+            </div>
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    flexWrap: isSmallerDisplay ? 'nowrap' : 'wrap',
+                }}>
+                <div>
+                    <div
+                        style={{
+                            marginTop: isSmallerDisplay ? '52px' : '32px',
+                            color: '#a5a5a5',
+                            fontSize: '18px',
+                        }}>
+                        {constants.SUCCESSFULLY_JOINED}{' '}
+                        <span
+                            style={{
+                                color: theme.palette.primary.main,
+                                fontWeight: 'bold',
+                            }}>
+                            {familyManagerEmail}
+                        </span>
+                        {constants.FAMILY_ON} <b>{constants.ENTE}</b>.
+                    </div>
+                    <div
+                        style={{
+                            marginTop: '24px',
+                            color: '#a5a5a5',
+                            fontSize: '18px',
+                        }}>
+                        {constants.YOU_NOW_HAVE_ACCESS}{' '}
+                        <span
+                            style={{
+                                color: '#fff',
+                                fontWeight: 'bold',
+                            }}>
+                            {convertBytesToHumanReadable(totalStorage)}
+                        </span>{' '}
+                        {constants.OF_SHARED_STORAGE}
+                    </div>
+                    <div
+                        style={{
+                            marginTop: '24px',
+                            color: '#a5a5a5',
+                            fontSize: '18px',
+                        }}>
+                        {constants.PLEASE_OPEN_ENTE} <b>{constants.ENTE}</b>{' '}
+                        {constants.BACKUP_PHOTOS_AND_VIDEOS}
+                    </div>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={handleClick}
+                        sx={{
+                            textTransform: 'none',
+                            fontWeight: 'bold',
+                            fontSize: '16px',
+                            marginTop: '30px',
+                            width: '200px',
+                        }}>
+                        {constants.OPEN_ENTE}
+                    </Button>
+                </div>
+                <ImageContainer mq={isSmallerDisplay}>
+                    <div
+                        style={{
+                            width: isSmallerDisplay ? '500px' : '400px',
+                            maxWidth: '90vw',
+                            objectFit: 'contain',
+                        }}>
+                        <img
+                            src="images/family_sharing.png"
+                            height={'100%'}
+                            width={'100%'}></img>
+                    </div>
+                </ImageContainer>
+            </div>
+        </Container>
     );
 }
 
