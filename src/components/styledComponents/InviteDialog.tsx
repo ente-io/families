d8dc79a66bc2e5a63bb45550c4792a4a3c9bc427
Rushikesh Tote <rushi.tote@gmail.com>
2022-05-08 00:16:08 +0530

    styling invite dialog

--- src/components/styledComponents/InviteDialog.tsx
+++ src/components/styledComponents/InviteDialog.tsx
@@ -1,6 +1,6 @@
 import { styled } from '@mui/material';
 
-export const ImageContainer = styled('div')<{ mq: boolean; }>(({ mq }) => ({
+export const ImageContainer = styled('div')<{ mq: boolean }>(({ mq }) => ({
     marginRight: mq ? '52px' : '48px',
     display: 'flex',
     flexDirection: 'column',
@@ -8,6 +8,15 @@ export const ImageContainer = styled('div')<{ mq: boolean; }>(({ mq }) => ({
     justifyContent: 'center',
     marginBottom: '-16px',
 }));
+
+export const CloseButtonContainer = styled('div')(() => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    marginRight: '-14px',
+    marginTop: '-8px',
+}));
+
 export const TextContainer = styled('div')(() => ({
     display: 'flex',
     flexDirection: 'column',
@@ -18,4 +27,12 @@ export const TextContainer = styled('div')(() => ({
     color: '#9f9f9f',
     fontSize: '12px',
     width: '90%',
+}));
+
+export const ErrorContainer = styled('div')(() => ({
+    fontWeight: 500,
+    fontSize: '12px',
+    marginTop: '10px',
+    width: '90%',
+    textAlign: 'center',
 }));
