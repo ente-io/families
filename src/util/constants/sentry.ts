9d7d94f031f57b492ad059408c38d0c49a5cf12d
Abhinav <abhinavk.grd@gmail.com>
2024-02-09 16:24:49 +0530

    update families

--- src/util/constants/sentry.ts
+++ src/util/constants/sentry.ts
@@ -1,6 +1,6 @@
 export const getSentryDSN = () =>
     process.env.NEXT_PUBLIC_SENTRY_DSN ??
-    'https://65eda0e72916645fbf6777880498c58f@sentry.ente.io/7';
+    'https://9e9d1082eca2168e1a4221cbc51eb421@sentry.ente.io/7';
 
 export const getSentryENV = () =>
     process.env.NEXT_PUBLIC_SENTRY_ENV ?? 'development';
