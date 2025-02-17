e13dc108422c2b7c518fb26986201c5b292cbcec
Manav Rathi <manav@mnvr.in>
2025-02-17 17:44:07 +0530

    Unsentry

--- next.config.js
+++ next.config.js
@@ -1,17 +1,1 @@
-const { withSentryConfig } = require('@sentry/nextjs');
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
-
-const { getGitSha, getIsSentryEnabled } = require('./configUtil');
-
-const GIT_SHA = getGitSha();
-
-const IS_SENTRY_ENABLED = getIsSentryEnabled();
-
-module.exports = (phase) =>
-    withSentryConfig(
-        {},
-        {
-            release: GIT_SHA,
-            dryRun: phase === PHASE_DEVELOPMENT_SERVER || !IS_SENTRY_ENABLED,
-        }
-    );
+module.exports = {}
