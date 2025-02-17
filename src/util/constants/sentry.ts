0b6cbb1e33cebc6e49265422ae2ebfbea631ec5f
mngshm <mangeshrex@tutanota.com>
2025-02-17 16:52:55 +0530

    update public Sentry DSN

--- src/util/constants/sentry.ts
+++ src/util/constants/sentry.ts
@@ -1,6 +1,6 @@
 export const getSentryDSN = () =>
     process.env.NEXT_PUBLIC_SENTRY_DSN ??
-    'https://ea6210a7e628d0d1457fd8e74a01d2bf@sentry.ente.io/7';
+    ' https://4ae5b1e889884dcac6c2e54c812cc8a2@sentry.ente.io/4';
 
 export const getSentryENV = () =>
     process.env.NEXT_PUBLIC_SENTRY_ENV ?? 'development';
