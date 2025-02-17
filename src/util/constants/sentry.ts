f1c7fd775f81152023fcb46e180e3408f4485d04
mngshm <mangeshrex@tutanota.com>
2025-02-17 17:13:24 +0530

    trailing whitespace

--- src/util/constants/sentry.ts
+++ src/util/constants/sentry.ts
@@ -1,6 +1,6 @@
 export const getSentryDSN = () =>
     process.env.NEXT_PUBLIC_SENTRY_DSN ??
-    ' https://4ae5b1e889884dcac6c2e54c812cc8a2@sentry.ente.io/4';
+    'https://4ae5b1e889884dcac6c2e54c812cc8a2@sentry.ente.io/4';
 
 export const getSentryENV = () =>
     process.env.NEXT_PUBLIC_SENTRY_ENV ?? 'development';
